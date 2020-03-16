ea66793a0f1a228f16ffc896d8b65fc06a9b0c70
anguie288 <61396665+anguie288@users.noreply.github.com>
2020-03-16 14:03:24 -0400

    Add files via upload

---
+++ pro/src/app/Main.java
@@ -0,0 +1,18 @@
+
+
+import dir.finder;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        finder myFinder = new finder();
+        
+        myFinder.find();
+        
+
+        System.out.println("good");
+    }
+}
---
+++ pro/src/app/dir/finder.java
@@ -0,0 +1,13 @@
+package dir;
+
+
+public class finder {
+
+    
+    public void find() {
+        
+        System.out.println("test");
+        
+    }
+
+}
